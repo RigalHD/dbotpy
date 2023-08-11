82a32f0702724b1dad24061f821f3069d30dd4a0
RigalHD <103758149+RigalHD@users.noreply.github.com>
2023-08-12 00:05:06 +0300

    Delete Dbot_modals.py

--- Dbot_modals.py
+++
@@ -1,9 +0,0 @@
-import disnake
-from disnake.ext import commands
-from disnake.ui import Button, View, Modal
-from Dbot import bot
-from typing import Optional
-from disnake import TextInputStyle
-from typing import Optional
-from disnake.interactions import MessageInteraction
-
